7b5f78b48b4c98b5635fbbe95fa26ff667b8a9ef
BarnabePortier <60656935+BarnabePortier@users.noreply.github.com>
2021-09-28 12:42:43 -0400

    Update Particule.h

--- MoteurPhysique/src/Particule.h
+++ MoteurPhysique/src/Particule.h
@@ -5,9 +5,11 @@ class Particule
 {
 private :
 
+	//inverse masse et frottement sont des constantes nécessaires au calcul de vitesse et accélération en physique
 	float inverseMasse;
 	float frottement;
 
+	//trois vecteurs des nécessaire à décrire l'état d'une particule et prévoir son évolution
 	Vector3D position;
 	Vector3D vitesse;
 	Vector3D acceleration;
@@ -20,8 +22,7 @@ public :
 
 	Particule(float invMasse, float frottement);
 
-	Particule(Vector3D initialPosition, Vector3D initialVitesse, Vector3D initialAcceleration);
-
+	//avec position, vitesse et accélération, suffisant à décrire la particule et son évolution
 	Vector3D appliedForce;
 
 	float getInverseMasse();
@@ -34,6 +35,7 @@ public :
 
 	void updateAcceleration();
 
+	//dans les trois fonctions suivantes, le temps est le delta temps avec l'instant précédent, permet une approximation du calcul
 	void updateVitesse(float temps);
 
 	void updatePosition(float temps);
@@ -42,6 +44,7 @@ public :
 
 
 };
+
 
 
 
