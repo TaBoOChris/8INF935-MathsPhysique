9048ad2f8c8ffc5aec2aaab3e91a43a46390621f
BarnabePortier <60656935+BarnabePortier@users.noreply.github.com>
2021-10-27 10:16:40 -0400

    particle contact

---
+++ src/maths/ParticleContact.cpp
@@ -0,0 +1,62 @@
+#include "ParticleContact.h"
+
+ParticleContact::ParticleContact(Particule* partA, Particule* partB, float restCoeff, Vector3D normal)
+{
+	this->particules[0] = partA;
+	this->particules[1] = partB;
+	this->restitution = restCoeff;
+	this->contactNormal = normal;
+}
+
+void ParticleContact::resolve(float duree)
+{
+	resolveInterpenetration();
+	resolveVelocity(duree);
+}
+
+float ParticleContact::caculVs() const
+{
+	return (particules[0]->getVitesse() - particules[1]->getVitesse())*contactNormal;
+}
+
+void ParticleContact::resolveVelocity(float duree)
+{
+	Particule* partA = this->particules[0];
+	Particule* partB = this->particules[1];
+
+	float Vs = caculVs();
+	float Vsp = -this->restitution * Vs;
+	Vector3D impulsionA = Vsp * this->contactNormal;
+	Vector3D impulsionB = -Vsp * this->contactNormal;
+
+	Vector3D vitA = partA->getVitesse() + partA->getInverseMasse() * impulsionA;
+	this->particules[0]->setVitesse(vitA);
+
+	Vector3D vitB = partB->getVitesse() + partB->getInverseMasse() * impulsionB;
+	this->particules[1]->setVitesse(vitB);
+}
+
+void ParticleContact::resolveInterpenetration()
+{
+	float invMasseA = this->particules[0]->getInverseMasse();
+	float invMasseB = this->particules[1]->getInverseMasse();
+
+	// on isole les cas nuls, dans tout les autres on inverse invMasse pour obtenir la masse utilisée dans la formule
+
+	if (invMasseA == 0) {
+		this->particules[1]->setPosition(this->particules[1]->getPosition() - this->penetration * this->contactNormal);
+	}
+	else if (invMasseB == 0) {
+		this->particules[0]->setPosition(this->particules[0]->getPosition() + this->penetration * this->contactNormal);
+	}
+	else {
+		float masseA = 1 / invMasseA;
+		float masseB = 1 / invMasseB;
+
+		float deltapA = masseA / (masseA + masseB) * penetration;
+		float deltapB = masseB / (masseA + masseB) * penetration;
+
+		this->particules[0]->setPosition(this->particules[0]->getPosition() + deltapA * this->contactNormal);
+		this->particules[1]->setPosition(this->particules[1]->getPosition() - deltapB * this->contactNormal);
+	}
+}
---
+++ src/maths/ParticleContact.h
@@ -0,0 +1,20 @@
+#include "Particule.h"
+#include "Vecteur3D.h"
+
+class ParticleContact
+{
+private:
+	Particule* particules[2];
+	float restitution;
+	Vector3D contactNormal;
+	float penetration;
+
+public:
+	ParticleContact(Particule* partA, Particule* partB, float restCoeff, Vector3D normal);
+
+	void resolve(float duree);
+	float caculVs() const;
+	void resolveVelocity(float duree);
+	void resolveInterpenetration();
+};
+
