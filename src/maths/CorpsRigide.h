61af798f83fb39c132550f35f473f9eaaa948688
Florent <florent.bogacz@gmail.com>
2021-11-17 13:15:13 -0500

    Mise en place du .h

--- src/maths/CorpsRigide.h
+++ src/maths/CorpsRigide.h
@@ -4,7 +4,40 @@
 /// 
 /// </summary>
 
+
+#include "Vecteur3D.h"
+#include "Quaternion.h"
+#include "Matrix3.h"
+
 class CorpsRigide
 {
+private:
+	float inverseMass;
+	float linearDamping;
+	Vector3D position;
+	Vector3D velocite;
+	Vector3D acceleration;
+	Quaternion orientation;
+	Vector3D rotation;			//velicte angulaire
+
+	Matrix3 transformMatrix;
+
+	Matrix3 inverseInertiaTensor;
+
+	float angularDamping;
+	
+
+public:
+
+	void calculDonneesDerivee();
+
+	void forceAccum();
+	void torqueAccum();
+	void clearAccumulators();
+
+	void addForceAtPoint(Vector3D force, Vector3D point);
+	void addForceAtBodyPoint(Vector3D force, Vector3D point);
+
+	
 };
 
