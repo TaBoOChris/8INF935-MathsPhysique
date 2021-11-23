c53fcbdd09b340cbb0fed608ab1a27ffd146b9ef
Florent <florent.bogacz@gmail.com>
2021-11-22 22:43:10 -0500

    Creation FormeIrregu

---
+++ src/maths/FormeIrreguliere.cpp
@@ -0,0 +1,13 @@
+#include "FormeIrreguliere.h"
+
+FormeIrreguliere::FormeIrreguliere()
+{
+	pt1 = Vector3D(-1, -1, -1);
+	pt2 = Vector3D(-1, -1, 1);
+	pt3 = Vector3D(-1, 1, 1);
+	pt4 = Vector3D(-1, 1, -1);
+	pt5 = Vector3D(1, -1, -1);
+	pt6 = Vector3D(1, -1, 1);
+	pt7 = Vector3D(1, 1, 1);
+	pt8 = Vector3D(1, 1, -1);
+}
---
+++ src/maths/FormeIrreguliere.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Vecteur3D.h"
+
+
+class FormeIrreguliere
+{
+public :
+
+	Vector3D pt1;
+	Vector3D pt2;
+	Vector3D pt3;
+	Vector3D pt4;
+	Vector3D pt5;
+	Vector3D pt6;
+	Vector3D pt7;
+	Vector3D pt8;
+
+
+	FormeIrreguliere();
+};
+
