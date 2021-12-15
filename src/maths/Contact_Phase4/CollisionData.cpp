872830c651f54ebc89c81e0ab3a9f4b089727c39
Florent <florent.bogacz@gmail.com>
2021-12-14 20:42:55 -0500

    upgrade printContact_console

--- src/maths/Contact_Phase4/CollisionData.cpp
+++ src/maths/Contact_Phase4/CollisionData.cpp
@@ -11,9 +11,13 @@ void CollisionData::generateContact(Primitive prim1, Primitive prim2, CollisionD
 
 void CollisionData::printContact_console()
 {
+	int index = 1;
 	for (Contact* contact : contacts)
 	{
+		std::cout << "contact " << index << " : " << std::endl;
 		std::cout << contact << std::endl;
 		std::cout << "\n" << std::endl;
+
+		index++;
 	}
 }
